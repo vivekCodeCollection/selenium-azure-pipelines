f5838be80377e9794a6d37c6bbf48fc2c9c30201
vivek kumar jha <30635319+vivekCodeCollection@users.noreply.github.com>
2025-01-18 00:03:03 +0530

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,12 +1,9 @@
+pip install webdriver-manager
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
-# Set up the Chrome driver using webdriver_manager
-service = Service(ChromeDriverManager().install())
-
-# Initialize the Chrome browser
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # Open a website
 driver.get("https://www.example.com")
