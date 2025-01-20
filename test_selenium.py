143bf5cfb2b37849c6514e0abaf59b90b34849f7
vivek kumar jha <30635319+vivekCodeCollection@users.noreply.github.com>
2025-01-20 12:02:14 +0530

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # If you want it to run headlessly
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-software-rasterizer")  # to avoid crashes in a container
+chrome_options.add_argument("--remote-debugging-port=9222")  # Optional debugging
 
-# Open a website
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
 driver.get("https://www.example.com")
-
-# Perform any actions you need here
-
-# Close the browser
+print(driver.title)
 driver.quit()
